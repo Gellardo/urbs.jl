5c7f57cb5b1db46486b8c947f0e4ab0ad5e8534d
Frank Schmidt <rubiccuber@googlemail.com>
2016-04-22 16:56:32 +0200

    fix travis-build
    install xlrd before installing
    removed nightly builds because of incompatibility with Excel Reader

--- src/urbs.jl
+++ src/urbs.jl
@@ -3,13 +3,15 @@ module urbs
 using ExcelReaders
 using DataFrames
 
-file = openxl("test/left-right.xlsx")
+function main(filename)
+	file = openxl(filename)
 
-commodities = readxl(DataFrame,file, "Commodity!A1:C3", header=true)
-processes = readxl(DataFrame,file, "Process!A1:B3", header=true)
-processCommodity = readxl(DataFrame,file, "Process-Commodity!A1:D3", header=true)
-demand = readxl(DataFrame, file, "Demand!A1:C5", header=true)
+	commodities = readxl(DataFrame,file, "Commodity!A1:C3", header=true)
+	processes = readxl(DataFrame,file, "Process!A1:B3", header=true)
+	processCommodity = readxl(DataFrame,file, "Process-Commodity!A1:D3", header=true)
+	demand = readxl(DataFrame, file, "Demand!A1:C5", header=true)
 
-println(commodities)
+	println(commodities)
+end # main()
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,6 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+filename = normpath(Pkg.dir("urbs"),"test", "left-right.xlsx")
+urbs.main(filename)
