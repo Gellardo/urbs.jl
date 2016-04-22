1fa6db5cde202710f2df0c3bc1c101a683300fec
Frank Schmidt <rubiccuber@googlemail.com>
2016-04-22 16:40:43 +0200

    basic implementation of excel reading

---
+++ src/urbs.jl
@@ -0,0 +1,15 @@
+module urbs
+
+using ExcelReaders
+using DataFrames
+
+file = openxl("test/left-right.xlsx")
+
+commodities = readxl(DataFrame,file, "Commodity!A1:C3", header=true)
+processes = readxl(DataFrame,file, "Process!A1:B3", header=true)
+processCommodity = readxl(DataFrame,file, "Process-Commodity!A1:D3", header=true)
+demand = readxl(DataFrame, file, "Demand!A1:C5", header=true)
+
+println(commodities)
+
+end # module
